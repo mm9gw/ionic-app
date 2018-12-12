64b2cdf959ced3610669e6e26766b158c99aff52
Meeka Meng <mm9gw@virginia.edu>
2018-12-12 10:55:23 -0500

    implemented ionic storage to store date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { DataProvider } from '../providers/data/data';
+import {IonicStorageModule} from '@ionic/storage';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { DataProvider } from '../providers/data/data';
   imports: [
     BrowserModule,
     HttpClientModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { AlertController } from 'ionic-angular';
 import { DataProvider } from '../../providers/data/data';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -10,35 +11,50 @@ import { Observable } from 'rxjs/Observable';
 })
 export class HomePage {
 
-  constructor(public alertCtrl: AlertController, public dataProvider: DataProvider) {
+  constructor(public alertCtrl: AlertController, public dataProvider: DataProvider, public storage: Storage) {
   }
 
   public clicked = false;
-  public day;
+  public day = '';
   showAlert() {
     let rand = (Math.floor(Math.random()*15));
     let today = new Date();
     let d = today.getDate();
     let m = today.getMonth()+1; //January is 0!
     let y = today.getFullYear();
-    today = m+'/'+d+'/'+y;
 
-    if(this.day!=today){
-      this.dataProvider.getRemoteData().subscribe(result => {
-      	this.clicked=true;
-        this.day=today;
-  		  this.dataProvider.item=result[rand].messages;
-      	console.log(result);
-      });
-    }
-    else{
-      const alert = this.alertCtrl.create({
-        title: 'Alert!',
-        subTitle: 'You have already generated the message for today',
-        buttons: ['OK']
-      });
-      alert.present();
-    }
+    //let todayString = m+'/'+d+'/'+y;
+    let todayString = '';
+    //this.storage.set('dateString', todayString);
+
+    
+    this.storage.get('day').then((val) => {
+      if(val!=todayString){
+        this.dataProvider.getRemoteData().subscribe(result => {
+      	  this.clicked=true;
+          //this.day=val;
+          this.storage.set('day',todayString);
+          console.log('The date is', val);
+  		    this.dataProvider.item=result[rand].messages;
+          this.storage.set('number',rand);
+          console.log(result);
+        });
+      }
+      else{
+        this.dataProvider.getRemoteData().subscribe(result => {
+          this.clicked=true;
+          this.storage.get('number').then((num) =>{
+            this.dataProvider.item=result[num].messages;
+          });
+        });
+        const alert = this.alertCtrl.create({
+          title: 'Alert!',
+          subTitle: 'You have already generated the message for today',
+          buttons: ['OK']
+        });
+        alert.present();
+      }
+    });
 
     //alert.present();
   }
